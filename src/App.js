4c3ec9c47c074b51b9d9f37f32682d133ef562cd
Thong <thonglaanh1011@gmail.com>
2023-08-25 21:19:23 +0700

    Add new pakage src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-      <p>hahaha</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
